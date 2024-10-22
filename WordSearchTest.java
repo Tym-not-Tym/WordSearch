0f85e45459e5e50c1b1e6814b9fcd1137a80be1d
Tym Grebenyuk <tima.grebenyuk@gmail.com>
2024-10-21 20:18:10 -0700

    added menu input int validation

--- WordSearchTest.java
+++ WordSearchTest.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class WordSearchTest{
@@ -5,14 +6,23 @@ public class WordSearchTest{
     public static void main(String[] args) {
         WordSearch myWordSearch = new WordSearch();
         Scanner scn = new Scanner(System.in);
-        int input;
+        int input = 0;
         do {
             System.out.println("'1' - Create a crossword puzzle");
             System.out.println("'2' - View a crossword puzzle with solutions");
             System.out.println("'3' - View a crossword puzzle without solutions");
             System.out.println("'4' - Quit");
 
-            input = scn.nextInt();
+            boolean flag = false;
+            do{
+                try {
+                    input = scn.nextInt();
+                    flag = true;
+                } catch (InputMismatchException ime) {
+                    System.out.println("Enter an Integer!");
+                    scn.nextLine();
+                }
+            } while (!flag);
 
             switch (input) {
                 case 1:
